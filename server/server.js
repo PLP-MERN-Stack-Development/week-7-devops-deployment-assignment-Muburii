34ab7935462aba7f6c98061468379b0e1b7e78ca
Zuffcodes <zuffcodes@kali.Zuffcodes.com>
2025-07-18 22:42:19 +0300

    Updated Cors

--- server/server.js
+++ server/server.js
@@ -6,11 +6,28 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://week-7-devops-deployment-assignment-mcjf.onrender.com",
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Serve running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
