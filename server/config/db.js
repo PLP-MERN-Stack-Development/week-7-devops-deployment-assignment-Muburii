3f0a5c400a3b4b6e07aba080d6363bd623ed12ca
Zuffcodes <zuffcodes@kali.Zuffcodes.com>
2025-07-18 18:03:40 +0300

    Updated Mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-//Connect to MongoDB using mongoose
-const  connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.log("MongoDb connection failed:", error.message);
-        process.exit(1);
-    }
-    };
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected successfully!');
+  } catch (error) {
+    console.error(`MongoDb connection failed: ${error.message}`);
+    process.exit(1);
+  }
+};
 module.exports = connectDB;
